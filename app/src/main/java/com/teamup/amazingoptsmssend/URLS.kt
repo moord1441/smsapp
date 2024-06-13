91d38eeafbd08ea03f953cbd684c3bca1909b0a8
moord1441 <mohammedayman.moord@gmail.com>
2024-06-13 11:54:56 +0200

    Delete app directory

--- app/src/main/java/com/teamup/amazingoptsmssend/URLS.kt
+++
@@ -1,6 +0,0 @@
-package com.teamup.amazingoptsmssend
-
-object URLS {
-
-   var  MAINURL = "https://script.google.com/macros/s/AKfycbzk4uYbjwiHzlAodajCmzUpTJLGYOWIVYfoZajJHIPmkknRsIzfevxyhC7_4Behxfhxgg/"
-}
